a77f19ac90852efd2598733918f01a9d63cfa4e9
Joe Vacovsky Jr <vacovsky@gmail.com>
2015-10-11 23:41:13 -0700

    Dawd

--- predfangy_api/predfangy.py
+++ predfangy_api/predfangy.py
@@ -83,7 +83,7 @@ def rexpsecs():
 
 @app.route('/rruncommand', methods=['POST'])
 def rruncommand():
-    result = r.execute_command(request.json['command'].encode('utf8'))
+    result = r.execute_command(bytes(request.json['command']))
     return jsonify({"return": result.decode('utf8')})
 
 
