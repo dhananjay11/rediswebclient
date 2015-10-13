86a198f70218a4354d01bb418212c59bde3e53fe
Joe Vacovsky Jr <vacovsky@gmail.com>
2015-10-12 19:44:56 -0700

    Gunicorn!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn predfangy:app --log-file=-
--- stupid.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-r = redis.StrictRedis(host='192.168.1.117', port=6379, db=0)
-def rruncommand(c):
-    result = r.execute_command(c)
-    return {"return": result.decode('utf8')}
-
-
-print(rruncommand("set fart poop"))
